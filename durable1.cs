e09ae3b122f5e985ed39135a4a8107fdc28320b1
Matt <macavall@microsoft.com>
2025-04-21 12:55:52 -0500

    first

--- durable1.cs
+++ durable1.cs
@@ -3,6 +3,7 @@ using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.DurableTask;
 using Microsoft.DurableTask.Client;
 using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -18,22 +19,43 @@ namespace DurableRepro2
             logger.LogInformation("Saying hello.");
             var outputs = new List<string>();
 
+            try
+            {
+                await context.CallActivityAsync(nameof(SayHello), "my custom exception message");
+            }
+            catch (TaskFailedException ex) // namespace: 'Microsoft.DurableTask' ... not 'DurableTask.Core.Exceptions'
+            {
+                Console.WriteLine(ex.Message);
+            }
+
             // Replace name and input with values relevant for your Durable Functions Activity
-            outputs.Add(await context.CallActivityAsync<string>(nameof(SayHello), "Tokyo"));
-            outputs.Add(await context.CallActivityAsync<string>(nameof(SayHello), "Seattle"));
-            outputs.Add(await context.CallActivityAsync<string>(nameof(SayHello), "London"));
+            //outputs.Add(await context.CallActivityAsync<string>(nameof(SayHello), "Tokyo"));
+            //outputs.Add(await context.CallActivityAsync<string>(nameof(SayHello), "Seattle"));
+            //outputs.Add(await context.CallActivityAsync<string>(nameof(SayHello), "London"));
 
             // returns ["Hello Tokyo!", "Hello Seattle!", "Hello London!"]
             return outputs;
         }
 
-        [Function(nameof(SayHello))]
-        public static string SayHello([ActivityTrigger] string name, FunctionContext executionContext)
+        public class CustomException : Exception
         {
-            ILogger logger = executionContext.GetLogger("SayHello");
-            logger.LogInformation("Saying hello to {name}.", name);
-            return $"Hello {name}!";
+            public CustomException() { }
+            public CustomException(string message) : base(message) { }
+            public CustomException(string message, Exception innerException) : base(message, innerException) { }
         }
+
+        [Function(nameof(SayHello))]
+        public static string SayHello([ActivityTrigger] string exceptionMessage, FunctionContext executionContext)
+        {
+            // only a single exception is actually thrown, but both variations are shown here for clarity
+            throw new CustomException();                 // results in Excpetion.FailureDetails.ErrorType being populated with the exception name
+            throw new CustomException(exceptionMessage); // results in Excpetion.FailureDetails.ErrorMessage being populated with the exception message
+        }
+        //{
+        //    ILogger logger = executionContext.GetLogger("SayHello");
+        //    logger.LogInformation("Saying hello to {name}.", name);
+        //    return $"Hello {name}!";
+        //}
 
         [Function("durable1_HttpStart")]
         public static async Task<HttpResponseData> HttpStart(
